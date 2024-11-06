b4e93d3696fb2a11b9bc48f734efc381a79c1831
Алексей Куликов <raylop@MacBook-Pro-Aleksej.local>
2024-11-06 15:50:56 +0900

    Initial commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,6 +7,40 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///transactions.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Регулярное выражение для поиска "ЗП" в любом регистре
+pattern = re.compile(r'\bзп\b', re.IGNORECASE)
+
+# Подсчёт балансов (все транзакции учитываются, включая расходы с "ЗП")
+def calculate_balances():
+    balances = {key: 0 for key in accounts.keys()}
+    transactions = Transaction.query.all()  # Получаем все транзакции
+    for transaction in transactions:
+        if transaction.type == 'income':
+            balances[transaction.account] += transaction.amount
+        elif transaction.type == 'expense':
+            # Все расходы учитываются, включая с "ЗП", для расчета балансов
+            balances[transaction.account] -= transaction.amount
+    return balances
+
+# Подсчёт общего дохода (все доходы учитываются)
+def calculate_total_income():
+    total_income = 0
+    transactions = Transaction.query.filter_by(type='income').all()
+    for transaction in transactions:
+        total_income += transaction.amount
+    return total_income
+
+# Подсчёт общего расхода (фильтруем расходы с "ЗП" из расчета)
+def calculate_total_expense():
+    total_expense = 0
+    transactions = Transaction.query.filter_by(type='expense').all()
+    for transaction in transactions:
+        # Исключаем расходы с "ЗП" из общего расчета
+        if not pattern.search(transaction.description):  
+            total_expense += transaction.amount
+    return total_expense
+
+# Модель для транзакций
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     account = db.Column(db.String(50), nullable=False)
@@ -14,7 +49,15 @@ class Transaction(db.Model):
     description = db.Column(db.String(100), nullable=False)
     date = db.Column(db.String(10), nullable=False)
 
-# Добавляем 4 счета
+# Модель для ожидаемых приходов
+class ExpectedIncome(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    account = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.String(10), nullable=False)
+
+# Счета
 accounts = {
     'account_1': 'Нал',
     'account_2': 'Перевод',
@@ -22,46 +65,58 @@ accounts = {
     'account_4': 'Макс'
 }
 
-# Подсчёт баланса по каждому счёту
-def calculate_balances():
-    balances = {key: 0 for key in accounts.keys()}
-    transactions = Transaction.query.all()
-    for transaction in transactions:
-        if transaction.type == 'income':
-            balances[transaction.account] += transaction.amount
-        elif transaction.type == 'expense':
-            balances[transaction.account] -= transaction.amount
-    return balances
-
+# Главная страница
 @app.route('/')
 def index():
-    transactions = Transaction.query.all()
-    balances = calculate_balances()
-    return render_template('index.html', transactions=transactions, accounts=accounts, balances=balances)
+    transactions = Transaction.query.all()  # Получаем все транзакции
+    balances = calculate_balances()  # Подсчитаем балансы (включая расходы с "ЗП")
+    expected_incomes = ExpectedIncome.query.all()
+    total_income = calculate_total_income()  # Подсчитаем общий доход
+    total_expense = calculate_total_expense()  # Подсчитаем общий расход (без "ЗП")
+    
+    return render_template(
+        'index.html',
+        transactions=transactions,  # Отображаем все транзакции
+        accounts=accounts,
+        balances=balances,
+        expected_incomes=expected_incomes,
+        total_income=total_income,
+        total_expense=total_expense
+    )
 
+# Добавить транзакцию
 @app.route('/add_transaction', methods=['POST'])
 def add_transaction():
     account = request.form['account']
-    type = request.form['type']
+    trans_type = request.form['type']
     amount = float(request.form['amount'])
     description = request.form['description']
     date = request.form['date']
-
-    new_transaction = Transaction(account=account, type=type, amount=amount, description=description, date=date)
+    new_transaction = Transaction(account=account, type=trans_type, amount=amount, description=description, date=date)
     db.session.add(new_transaction)
     db.session.commit()
-
     return redirect(url_for('index'))
 
-@app.route('/delete_transaction/<int:transaction_id>', methods=['POST'])
-def delete_transaction(transaction_id):
-    transaction = Transaction.query.get(transaction_id)
-    if transaction:
-        db.session.delete(transaction)
-        db.session.commit()
+# Добавить ожидаемый приход
+@app.route('/add_expected_income', methods=['POST'])
+def add_expected_income():
+    account = request.form['account']
+    amount = float(request.form['amount'])
+    description = request.form['description']
+    date = request.form['date']
+    new_income = ExpectedIncome(account=account, amount=amount, description=description, date=date)
+    db.session.add(new_income)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# Удалить ожидаемый приход
+@app.route('/delete_expected_income/<int:id>', methods=['POST'])
+def delete_expected_income(id):
+    income = ExpectedIncome.query.get_or_404(id)
+    db.session.delete(income)
+    db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    db.create_all()
     app.run(debug=True)
